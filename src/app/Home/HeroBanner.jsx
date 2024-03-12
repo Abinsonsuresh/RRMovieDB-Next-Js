b8ce29cd7617b8fde2f9a467f1f12b505e426167
Abinson Suresh <88340846+Abinsonsuresh@users.noreply.github.com>
2024-03-12 23:38:10 +0530

    Hero banner search router added

--- src/app/Home/HeroBanner.jsx
+++ src/app/Home/HeroBanner.jsx
@@ -1,6 +1,6 @@
 'use client'
 import React, { useEffect, useState } from 'react'
-
+import { useRouter } from 'next/navigation'
 import useFetchAPI from '@/hooks/useFetchAPI'
 import { useSelector } from "react-redux"
 
@@ -8,7 +8,7 @@ const HeroBanner = () => {
     const [background, setBackground] = useState("");
     const [query, setQuery] = useState("");
     console.log(query)
-
+    const router = useRouter()
 
     const { url } = useSelector((state) => state.home) //imgurl inside the home slice
 
@@ -17,7 +17,11 @@ const HeroBanner = () => {
     console.log(data)
 
     const SearchHandler = (e) => {
+        e.preventDefault()
         if (e.key == "Enter" && query.length > 0) {
+           
+            router.push('/Search')
+            console.log("click ee")
             // navigate(`/search/${query}`)
         }
     }
@@ -31,7 +35,7 @@ const HeroBanner = () => {
                 
                 <div className="bgimg w-full h-full absolute top-0 left-0 opacity-[0.5] overflow-hidden">
                     <img className='w-full h-full object-cover object-center' src={background} alt="" />
-                    {/* <LazyLoad className="w-full h-full" src={background} /> */}
+                   
                 </div>
 
                 <div className="op w-full h-[250px]  absolute bottom-0 left-0">
@@ -43,6 +47,8 @@ const HeroBanner = () => {
                         <p className='text-4xl font-bold text-white'>Explore the World of Movies</p>
                     </div>
                     <div className=''>
+                        <form  onSubmit={SearchHandler}>
+
                         <input
                             type="text"
                             placeholder='Search Movies or TV Shows'
@@ -50,7 +56,9 @@ const HeroBanner = () => {
                             onKeyUp={SearchHandler}
                             className='p-2 border rounded'
                         />
-                        <button className='px-4 py-2 bg-blue-500 text-white ml-2'>Search</button>
+                        <button className='px-4 py-2 bg-blue-500 text-white ml-2' type='submit'>Search</button>
+
+                        </form>
                     </div>
                 </div>
             </div>
