c7a41f6392067ade27dc25615de164d01fd5acd0
Abinson Suresh <88340846+Abinsonsuresh@users.noreply.github.com>
2024-03-12 22:55:38 +0530

    Redux store and slice setuped

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import ReduxProvider from "@/provider/ReduxProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,8 +11,12 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
+    <ReduxProvider>
+
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
+
+    </ReduxProvider>
   );
 }
