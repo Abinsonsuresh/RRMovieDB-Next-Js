18139a102915e1480bb682bde7be7de795862311
Abinson Suresh <88340846+Abinsonsuresh@users.noreply.github.com>
2024-03-13 00:36:56 +0530

    Top rated section added

--- src/app/Home/MainHome.jsx
+++ src/app/Home/MainHome.jsx
@@ -1,12 +1,14 @@
 'use client'
 import HeroBanner from "./HeroBanner"
 import Popular from "./Popular"
+import TopRated from "./TopRated"
 
 const MainHome = () => {
   return (
    <div>
        <HeroBanner/>
        <Popular/>
+       <TopRated/>
 
    </div>
        
---
+++ src/app/Home/TopRated.jsx
@@ -0,0 +1,26 @@
+'use client'
+import React, { useEffect, useState } from 'react'
+import useFetchAPI from '@/hooks/useFetchAPI'
+import Caraousel from '@/Components/Caraousel'
+
+const TopRated = () => {
+  const [endpoint, setEndpoint] = useState("movie")
+
+
+  const {data, loading} = useFetchAPI(`/${endpoint}/top_rated`)
+  console.log("TopRated:",data)
+  return (
+    <div>
+      <>
+      <div>
+        <h2>TopRated Movies</h2>
+      </div>
+      <div>
+        <Caraousel data={data?.results} loading={loading} endpoint={endpoint}/>
+      </div>
+      </>
+    </div>
+  )
+}
+
+export default TopRated
