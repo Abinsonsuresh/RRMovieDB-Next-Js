af1b053bdce4fb7cbd878248c7dfad7c9044fb37
Abinsonsuresh <polocreations916@gmail.com>
2024-03-13 20:20:27 +0530

    movie card component added

---
+++ src/Components/MovieCard.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { MdAddToPhotos } from 'react-icons/md';
+import { useSelector } from 'react-redux';
+
+const MovieCard = ({data}) => {
+    const { url } = useSelector((state) => state.home)
+
+    const Type = (data?.media_type?.charAt(0)?.toUpperCase() + data?.media_type?.slice(1)) || '';
+    const posterUrl = data.poster_path ? url.poster + data.poster_path : "";
+console.log(url)
+  return (
+    <div  onClick={() => router.push(`/Details/${item.media_type || endpoint}/${item.id}`)} className='bg-primary relative shadow-2xl border border-secondary rounded-lg min-w-44 max-h-[400px] min-h-[400px]   cursor-pointer hover:scale-105 ease-in-out duration-300' >
+
+
+    <div className='absolute bg-[#424242c3] rounded-sm top-0 right-0 p-2 cursor-pointer hover:scale-105 ease-in-out duration-300'>
+        <MdAddToPhotos size={20} color='white' />
+    </div>
+
+
+    <div className='flex justify-center items-start flex-col'>
+        <div>
+            <img className='max-w-44 object-cover rounded-lg' src={posterUrl} alt="" />
+        </div>
+        <div className='text-primary p-2'>
+            <p className='font-bold '>{data?.title || data?.name}</p>
+            <p className='text-gray-500'>{Type || "Movie"}</p>
+            <div className='flex'>
+                {/* <GenresBlock data={item?.genre_ids.slice(0, 1)} /> */}
+            </div>
+
+        </div>
+    </div>
+
+</div>
+  )
+}
+
+export default MovieCard
--- src/app/Search/[query]/page.js
+++ src/app/Search/[query]/page.js
@@ -1,4 +1,6 @@
 'use client'
+import Caraousel from '@/Components/Caraousel';
+import MovieCard from '@/Components/MovieCard';
 import { APIData } from '@/utils/api';
 import React, { useEffect, useState } from 'react'
 
@@ -6,11 +8,13 @@ const page = ({params}) => {
     const [data, setData] = useState(null);
     const [pageNum, setPageNum] = useState(1);
     const [loading, setLoading] = useState(false);
+    console.log(pageNum)
 
     const FetchSearchData =()=>{
         APIData(`/search/multi?query=${params.query}&page=${pageNum}`).then(
             (res)=>{
                 setData(res)
+                // setPageNum((prev) => prev + 1);
                 console.log(res)
             }
         )
@@ -22,7 +26,24 @@ const page = ({params}) => {
     },[params.query])
     
   return (
-    <div>page</div>
+    <div>
+        <div>
+            darag
+        </div>
+        {
+            data?.results.map((item,index)=>{
+                if (item.media_type === "person") return;
+                return(
+                <div className='' key={index}>
+                    <MovieCard data={item} />
+
+                </div>
+
+                )
+
+            })
+        }
+    </div>
   )
 }
 
