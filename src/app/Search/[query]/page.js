e3e69287b7f772c9406e6d9fdfc84b3c7919f8c6
Abinson Suresh <88340846+Abinsonsuresh@users.noreply.github.com>
2024-03-14 00:27:38 +0530

    fix

--- src/app/Search/[query]/page.js
+++ src/app/Search/[query]/page.js
@@ -4,62 +4,62 @@ import MovieCard from '@/Components/MovieCard';
 import { APIData } from '@/utils/api';
 import React, { useEffect, useState } from 'react'
 
-const page = ({params}) => {
-    const [data, setData] = useState(null);
-    const [pageNum, setPageNum] = useState(1);
-    const [loading, setLoading] = useState(false);
+const page = ({ params }) => {
+    const [data, SetData] = useState(null);
+    const [pageNum, SetPageNum] = useState(1);
+    const [loading, SetLoading] = useState(false);
     console.log(pageNum)
 
-    const FetchSearchData =()=>{
+    const FetchSearchData = () => {
         APIData(`/search/multi?query=${params.query}&page=${pageNum}`).then(
-            (res)=>{
-                setData(res)
-                setPageNum((prev) => prev + 1);
+            (res) => {
+                SetData(res)
+                // setPageNum((prev) => prev + 1);
                 console.log(res)
-                setLoading(false)
+                SetLoading(false)
             }
         )
     }
 
-    const NextPage =()=>{
+    const NextPage = () => {
         APIData(`/search/multi?query=${params.query}&page=${pageNum}`).then(
-            (res)=>{
-                if(data?.results)
-                {
-                    setData({...data, results:[...data?.results,...res?.results]})
+            (res) => {
+                if (data?.results) {
+                    SetData({ ...data, results: [...data?.results, ...res?.results] })
                 }
-                else{
-                    setData(res)
+                else {
+                    SetData(res)
                 }
-                setPageNum((prev) => prev + 1);
+                // setPageNum((prev) => prev + 1);
                 console.log(res)
-               
+
             }
         )
     }
 
-    useEffect(()=>{
-        setPageNum(1)
+    useEffect(() => {
+        SetPageNum(1)
         FetchSearchData()
-    },[params.query])
-    
-  return (
-    <div className='flex justify-center items-center flex-wrap gap-5'>
-     
-        {
-            data?.results.map((item,index)=>{
-                if (item.media_type === "person") return;
-                return(
-                <div className='' key={index}>
-                    <MovieCard data={item} />
-                </div>
+    }, [params.query])
 
-                )
+    return (
+        <div className='flex justify-center items-center flex-wrap gap-5'>
 
-            })
-        }
-    </div>
-  )
+            {
+                data?.results.map((item, index) => {
+                    if (item.media_type === "person") return;
+                    return (
+                        <div className='' key={index}>
+                            <MovieCard data={item} />
+                        </div>
+
+                    )
+
+                })
+            }
+         
+        </div>
+    )
 }
 
 export default page
