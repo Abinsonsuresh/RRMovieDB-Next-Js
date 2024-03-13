e58026bbe0f98f2683c7d0783d8428e5ecd7c1db
Abinsonsuresh <polocreations916@gmail.com>
2024-03-13 20:23:42 +0530

    router fix

--- src/Components/MovieCard.jsx
+++ src/Components/MovieCard.jsx
@@ -1,15 +1,19 @@
+import { useRouter } from 'next/navigation'
 import React from 'react'
 import { MdAddToPhotos } from 'react-icons/md';
 import { useSelector } from 'react-redux';
 
 const MovieCard = ({data}) => {
     const { url } = useSelector((state) => state.home)
-
+const router = useRouter()
     const Type = (data?.media_type?.charAt(0)?.toUpperCase() + data?.media_type?.slice(1)) || '';
     const posterUrl = data.poster_path ? url.poster + data.poster_path : "";
 console.log(url)
   return (
-    <div  onClick={() => router.push(`/Details/${item.media_type || endpoint}/${item.id}`)} className='bg-primary relative shadow-2xl border border-secondary rounded-lg min-w-44 max-h-[400px] min-h-[400px]   cursor-pointer hover:scale-105 ease-in-out duration-300' >
+
+
+    
+    <div  onClick={() => router.push(`/Details/${data.media_type || endpoint}/${data.id}`)} className='bg-primary relative shadow-2xl border border-secondary rounded-lg min-w-44 max-h-[400px] min-h-[400px]   cursor-pointer hover:scale-105 ease-in-out duration-300' >
 
 
     <div className='absolute bg-[#424242c3] rounded-sm top-0 right-0 p-2 cursor-pointer hover:scale-105 ease-in-out duration-300'>
