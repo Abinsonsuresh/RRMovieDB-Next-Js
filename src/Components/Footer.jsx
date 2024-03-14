b55c7842381a926b450d591bded00587fbdad2a1
Abinson Suresh <88340846+Abinsonsuresh@users.noreply.github.com>
2024-03-14 17:15:22 +0530

    margin added on top

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -13,15 +13,16 @@ import {
 const Footer = () => {
     return (
         <>
+        <div className='mt-12 p-8'>
             <p className='text-center p-4 '>Copyright © 2024 RRMovieDB - Created by Abinson Suresh</p>
             <div className='flex items-center justify-center space-x-4'>
-
 
                 <a href="https://github.com/Abinsonsuresh/" target="_blank"> <FaGithub size={25} /></a>
                 <a href="https://www.linkedin.com/in/abinsonsuresh/" target="_blank"><FaLinkedin size={25} /></a>
                 <a href="https://twitter.com/abinsonsuresh/" target="_blank">  <FaTwitter size={25} /></a>
 
             </div>
+        </div>
 
         </>
     );
